ce97f25b56e66bccb549b0e09167d5641f1ebde6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-06 16:14:14 +0900

    refactor(util): move file

--- util.ts
+++ util.ts
@@ -1,82 +1,8 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-import {
-  ascend,
-  isNegativeNumber,
-  isNull,
-  isValidDate,
-  Method,
-  parseFieldValue,
-} from "./deps.ts";
-import { isStrong, strongMatch, weakMatch } from "./etag.ts";
+import { ascend, isNegativeNumber, isNull, Method } from "./deps.ts";
 import type { Precondition } from "./types.ts";
-
-export function ifNoneMatch(fieldValue: string, etag: string): boolean {
-  if (isStar(fieldValue)) return true;
-
-  return !parseFieldValue(fieldValue)
-    .every((opaqueTag) => weakMatch(opaqueTag, etag));
-}
-
-export function ifMatch(
-  fieldValue: string,
-  etag: string,
-): boolean {
-  if (isStar(fieldValue)) return true;
-
-  if (!isStrong(etag)) return false;
-
-  return parseFieldValue(fieldValue).some((opaqueTag) =>
-    strongMatch(opaqueTag, etag)
-  );
-}
-
-export function ifModifiedSince(
-  fieldValue: string,
-  lastModified: string,
-): boolean {
-  // A recipient MUST ignore the If-Modified-Since header field if the
-  // received field-value is not a valid HTTP-date
-  const date = new Date(fieldValue);
-  const lastModifiedDate = new Date(lastModified);
-
-  if (!isValidDate(date) || !isValidDate(lastModifiedDate)) {
-    return true;
-  }
-
-  // The origin server SHOULD NOT perform the requested
-  // method if the selected representation's last modification date is
-  // earlier than or equal to the date provided in the field-value;
-  // instead, the origin server SHOULD generate a 304 (Not Modified)
-  // response, including only those metadata that are useful for
-  // identifying or updating a previously cached response.
-  return lastModifiedDate > date;
-}
-
-export function ifUnmodifiedSince(
-  fieldValue: string,
-  lastModified: string,
-): boolean {
-  // A recipient MUST ignore the If-Modified-Since header field if the
-  // received field-value is not a valid HTTP-date
-  const date = new Date(fieldValue);
-  const lastModifiedDate = new Date(lastModified);
-
-  if (!isValidDate(date) || !isValidDate(lastModifiedDate)) return true;
-
-  // The origin server MUST NOT perform the requested method
-  // if the selected representation's last modification date is more
-  // recent than the date provided in the field-value; instead the origin
-  // server MUST respond with either a) the 412 (Precondition Failed)
-  // status code.
-  return lastModifiedDate <= date;
-}
-
-/** Whether the input is `*` or not. */
-function isStar(input: string): input is "*" {
-  return input === "*";
-}
 
 export function isNotSelectionOrModificationMethod(method: string): boolean {
   return ([Method.Connect, Method.Options, Method.Trace] as string[]).includes(
